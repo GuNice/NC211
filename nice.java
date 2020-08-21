b939daed55ad5c1c773a5de7bf29d5d2b661aad2
GuNice <69842041+GuNice@users.noreply.github.com>
2020-08-21 16:35:15 +0700

    Add files via upload

---
+++ nice.java
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<h1>สวัสดีชาวโลก</h1>
+<p>ครับ</p>
+<p>ชัยธวัช ทองเพชรบูรพา
+<p>สวัสดีครับผมชื่อ <br> ไนท์
+
+<h1>ประเทศไทย</h1>
+<a href="https://www.w3schools.com">Visit
+W3Schools</a>
+<p> รูป</p>
+<img src="https://sites.google.com/site/siriluk37127/10-prathesthiy-thailand/630_Thailand.gif?attredirects=0"alt="ประเทศไทย
+" width="200" height="200">
+<!DOCTYPE html>
+<body>
+<h1>หอการค้าไทย</h1>
+<h2>ชัยธชัย ทองเพชรบูรพา</h2>
+<h3>ไนท์</h3>
+<h4>1910711102006</h4>
+<h5>facebook</h5>
+<h6>ไนท์'ท์ ฮาว'ว</h6>
+</body>
+<h1 style="font-size:50px;">ประเทศไทย</h1>
+<h1>หิวข้าว</h1>
+<p>ไนท์</p>
+<p>ไนท์</p>
+<h1>ไนท์</h1>
+<p>ไนท์</p>
+<hr>
+<h2>ไนท์</h2>
+<p>ไนท์</p>
+<hr>
+<h2>ไนท์</h2>
+<p>ไนท์</p>
+<hr>
+<pre>
+  สวัสดัครับ
+  ผมชื่อ ชัยธวัช ทองเพชรบูรพา 
+  เลขประจำนักศึกษา 1910711102006
+</pre>
+<h1>ปะเทศไทย</h1>
+<p>ไนท์</p>
+<p style="color:red;">ไนท์</p>
+<p style="color:blue;">ไนท์</p>
+<p style="font-size:50px;color:green;">ไนท์</p>
+<body style="background-color:#b3b3ff;">
+<h1 style="background-color:powderblue;">ไนท์</h1>
+<p style="background-color:tomato;">ไนท์</p>
+<h1 style="font-family:verdana;">ไนท์</h1>
+<p style="font-family:courier;">ไนท์</p>
+<h1 style="font-size:301%;">ไนท์</h1>
+<p style="font-size:160%;">ไนท์</p>
+
+<h1 style="text-align:center;">ไนท์</h1>
+<p style="text-align:center;">ไนท์</p>
+<h1>ไนท์</h1>
+<p><i>ไนท์</i></p>
+
+<p><em>ไนท์</em></p>
+<small>ตัวอักษรเล็ก</small>
+<p>Do not forget to buy <mark>Nice</mark> today.</p>
+<p>My favorite color is <del>ขีดค่า</del> red.</p>
+<p>My favorite color is <del>blue</del> <ins>ขีดเส้นใต้</ins>.</p>
+<p>This is <sub>อักษรลงข้างล่าง</sub> text.</p>
+<p>This is <sup>อักษรขึ้นลอยขึ้นบน</sup> text.</p>
+<h1>ไนท์</h1>
+<blockquote cite="http://www.worldwildlife.org/who/index.html">เรียงตัวอักษรให้เป็นระเบียน
+For 50 years, WWF has been protecting the future of nature.
+The world's leading conservation organization,
+WWF works in 100 countries and is supported by
+1.2 million members in the United States and
+close to 5 million globally.
+</blockquote>
+<p>....<q>....</q></p>
+<h1>ประเทศไทย</h1>
+<!-- This is a comment -->
+<h1>ประเทศไทย</h1>
+<h1 style="background-color:rgb(255, 99, 71);">ไนท์</h1>
+<h1 style="background-color:#ff6347;">ไนท์</h1>
+<h1 style="background-color:hsl(9, 100%, 64%);">ไนท์</h1>
+<h1 style="background-color:rgba(255, 99, 71, 0.5);">ไนท์</h1>
+<h1 style="background-color:hsla(9, 100%, 64%, 0.5);">ไนท์</h1>
+<h1 style="border:2px solid Tomato;">ไนท์</h1>
+<h1 style="border:2px solid DodgerBlue;">ไนท์</h1>
+<h1 style="border:2px solid Violet;">ไนท์</h1>
+</body>
+</html>
